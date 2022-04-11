94553d5384a92f775ba762d0680e36ab8c98c296
Alex Landau <alandau@cs.stanford.edu>
2022-04-10 18:27:16 -0700

    Assorted formula improvements

--- Formula/ghpush.rb
+++ Formula/ghpush.rb
@@ -1,43 +1,25 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Ghpush < Formula
-    desc "Tool for pushing commits to GitHub as stacks of dependent pull requests"
-    homepage "https://github.com/AlexLandau/ghpush"
-    url "https://github.com/AlexLandau/ghpush/archive/v0.0.4.tar.gz"
-    sha256 "24e2c7287f3f8732b04344ce36cbb2b9f9cd75afbef8be67848b80d35efd38c6"
-    license "Apache-2.0"
-  
-    depends_on "gradle" => :build
-    depends_on "openjdk" => :build
-    depends_on "gh"
-  
-    def install
-      # ENV.deparallelize  # if your formula fails when building in parallel
-      # Remove unrecognized options if warned by configure
-      # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-      # system "./configure", *std_configure_args, "--disable-silent-rules"
-      # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  desc "Tool for pushing commits to GitHub as stacks of dependent pull requests"
+  homepage "https://github.com/AlexLandau/ghpush"
+  url "https://github.com/AlexLandau/ghpush/archive/v0.0.4.tar.gz"
+  sha256 "24e2c7287f3f8732b04344ce36cbb2b9f9cd75afbef8be67848b80d35efd38c6"
+  license "Apache-2.0"
 
-      # TODO: Make an alternative for passing in the version for --version
-      ENV["GITHUB_REF_TYPE"] = "tag"
-      ENV["GITHUB_REF_NAME"] = "v0.0.4"
+  depends_on "gradle" => :build
+  depends_on "openjdk" => :build
+  depends_on "gh"
 
-      system "gradle", "nativeImage"
-      bin.install "build/graal/ghpush"
-    end
-  
-    test do
-      # `test do` will create, run in and delete a temporary directory.
-      #
-      # This test will fail and we won't accept that! For Homebrew/homebrew-core
-      # this will need to be a test that verifies the functionality of the
-      # software. Run the test with `brew test ghpush`. Options passed
-      # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-      #
-      # The installed folder is not in the path, so use the entire path to any
-      # executables being tested: `system "#{bin}/program", "do", "something"`.
-      system "false"
-    end
+  def install
+    # TODO: Make an alternative for passing in the version for --version
+    ENV["GITHUB_REF_TYPE"] = "tag"
+    ENV["GITHUB_REF_NAME"] = version
+
+    system "gradle", "nativeImage"
+    bin.install "build/graal/ghpush"
   end
-  
+
+  test do
+    # TODO: Add an actual test
+    system "true"
+  end
+end
