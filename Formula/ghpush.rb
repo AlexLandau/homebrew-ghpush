98b55f8a41c668da107575f3c51c36b0c7ad8031
Alex Landau <alandau@cs.stanford.edu>
2022-04-10 18:06:26 -0700

    Add GitHub Actions from brew tap-new

--- Formula/ghpush.rb
+++ Formula/ghpush.rb
@@ -2,17 +2,15 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Ghpush < Formula
-    # TODO: Description
-    desc ""
+    desc "Tool for pushing commits to GitHub as stacks of dependent pull requests"
     homepage "https://github.com/AlexLandau/ghpush"
     url "https://github.com/AlexLandau/ghpush/archive/v0.0.4.tar.gz"
     sha256 "24e2c7287f3f8732b04344ce36cbb2b9f9cd75afbef8be67848b80d35efd38c6"
     license "Apache-2.0"
   
-    # depends_on "cmake" => :build
-    # TODO: Verify these
     depends_on "gradle" => :build
     depends_on "openjdk" => :build
+    depends_on "gh"
   
     def install
       # ENV.deparallelize  # if your formula fails when building in parallel
