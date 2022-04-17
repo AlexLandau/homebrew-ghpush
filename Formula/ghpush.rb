4c4bb301b07721de83dfbab769dbdfb64af49011
Alex Landau <5733346+AlexLandau@users.noreply.github.com>
2022-04-17 02:20:39 +0000

    ghpush: add 0.0.14 bottle.

--- Formula/ghpush.rb
+++ Formula/ghpush.rb
@@ -5,6 +5,12 @@ class Ghpush < Formula
   sha256 "0fa18b90ea9e81ed394f6bfe336ff14e265c2afd2f6b7b9f113509bc1f5fc5b8"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/AlexLandau/homebrew-ghpush/releases/download/ghpush-0.0.14"
+    sha256 cellar: :any,                 big_sur:      "0789a42ceaa5ac716ea24ebe10bc0a08a015be59b39b9448f8bb7df3961d3172"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "ead0dc0089999011c1e34f98a615397b3756fec8661f72b6645a265b88a2bee1"
+  end
+
   depends_on "gradle" => :build
   depends_on "openjdk" => :build
   depends_on "gh"
